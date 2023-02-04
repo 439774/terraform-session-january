c0d411b4376fcefe301f794c869a9260578438f8
439774 <therealkimbladi@gmail.com>
2023-02-04 12:07:01 -0600

    Added some changes to versioning

--- sesson-2/versions.tf
+++ sesson-2/versions.tf
@@ -1,12 +1,21 @@
 terraform {
-  required_version = "~> 0.14.0" # Terraform Version
+  required_version = "~> 1.3.7" # Terraform Version
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.24.0" # AWS Provider
+      version = "~> 4.53.0" # AWS Provider
     }
   }
 }
 
 # Terraform = v1.3.7
 # AWS Provider = v4.53.0
+
+# Semantic Versioning
+# v1.3.7
+# 1 = Major
+# 3 = Minor
+# 7 = Patch
+
+# ~> = lazy contraints
+# ~> 0.13 = 0.13.0 - 0.13.x

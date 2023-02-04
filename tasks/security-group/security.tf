62934549f9312eddf768540f96a89725954f6a7e
439774 <therealkimbladi@gmail.com>
2023-02-04 14:53:11 -0600

    testing

--- tasks/security-group/security.tf
+++ tasks/security-group/security.tf
@@ -6,6 +6,29 @@ resource "aws_security_group" "CreateSG" {
     to_port          = 22
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port        = -1
+    to_port          = -1
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
 }

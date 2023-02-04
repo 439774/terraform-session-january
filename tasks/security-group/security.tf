7a83f09d8a4e793e876bd2d1fa552739ae7f1b11
439774 <therealkimbladi@gmail.com>
2023-02-04 15:01:52 -0600

    some more

--- tasks/security-group/security.tf
+++ tasks/security-group/security.tf
@@ -25,10 +25,4 @@ resource "aws_security_group" "CreateSG" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-}
 }

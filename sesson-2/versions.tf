1da3003c2fc90f27b4c2654311b24b309dacb3e0
439774 <therealkimbladi@gmail.com>
2023-02-04 11:56:29 -0600

    edits

--- sesson-2/versions.tf
+++ sesson-2/versions.tf
@@ -1,9 +1,12 @@
 terraform {
-  required_version = "~> 0.14.0"
+  required_version = "~> 0.14.0" # Terraform Version
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.24.0"
+      version = "~> 3.24.0" # AWS Provider
     }
   }
 }
+
+# Terraform = v1.3.7
+# AWS Provider = v4.53.0

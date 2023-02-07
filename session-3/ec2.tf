8235b0b358ba1244dbaa18733e69f43fe9f83c5d
439774 <therealkimbladi@gmail.com>
2023-02-06 20:39:19 -0600

    changes

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "first_ec2" {
   ami                    = "ami-0aa7d40eeae50c9a9"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.main_sg.id ]
+  tags = {
+    Name = "development"
+  }
 }
 
 

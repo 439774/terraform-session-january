0e7424d330080755460613fb19d50d06447bf6b7
439774 <therealkimbladi@gmail.com>
2023-02-06 21:05:08 -0600

    adding variables

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "first_ec2" {
-  ami                    = "ami-0aa7d40eeae50c9a9"
-  instance_type          = "t2.micro"
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [ aws_security_group.main_sg.id ]
   tags = {
-    Name = "development"
+    Name = var.env
   }
 }
 
@@ -24,3 +24,5 @@ resource "aws_instance" "first_ec2" {
 # first_label.second_label.attribute 
 # Attributes are something that is known after the creation
 # Cannot be a string because References are dynamic not static
+# Reference to input variable
+# Syntax: var.VariableName
---
+++ session-3/variables.tf
@@ -0,0 +1,7 @@
+# Input Variables
+
+variable "instance_type" {
+    type = string
+    description = "This is an instance type for EC2 instances"
+    default = "t2.micro"
+}

dd87143dbca065026397e0d3b0792bd4c4921eaf
439774 <therealkimbladi@gmail.com>
2023-02-07 22:32:11 -0600

    cidr block errors fixed

--- tasks/vpc/subnets.tf
+++ tasks/vpc/subnets.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "pub_sub_a" {
 
 resource "aws_subnet" "pub_sub_b" {
   vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "pub_sub_b"
@@ -18,7 +18,7 @@ resource "aws_subnet" "pub_sub_b" {
 
 resource "aws_subnet" "pub_sub_c" {
   vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.3.0/24"
 
   tags = {
     Name = "pub_sub_c"

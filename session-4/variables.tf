c97df989e88e5ac5d21dad56f9655a66a32a74ae
439774 <therealkimbladi@gmail.com>
2023-02-08 18:41:56 -0600

    adding more files

--- session-4/variables.tf
+++ session-4/variables.tf
@@ -1,3 +1,27 @@
 variable "aws_region" {    
     default = "us-east-1"
 }
+
+# Input Variables
+
+variable "instance_type" {
+    type = string
+    description = "This is an instance type for EC2 instances"
+    default = "t2.micro"
+}
+
+variable "ami" {
+    type = string
+    description = "This is an instance type for EC2 instances"
+    default = "ami-0aa7d40eeae50c9a9"
+}
+
+variable "env" {
+    type = string
+    description = "This is an instance type for EC2 instances"
+    default = "dev"
+}
+
+variable "aws_region" {    
+    default = "us-east-1"
+}

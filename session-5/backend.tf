646ca1a5d05d695df4c7f39e5dbba555b160f9e7
439774 <therealkimbladi@gmail.com>
2023-02-11 11:35:18 -0600

    added state-lock file

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,5 +3,9 @@ terraform {
     bucket = "terraform-session-septemeber-backend-alisher"
     key    = "session-5/terraform.tfstate"  # Where does terraform need to store your file. Path Or Prefix
     region = "us-east-1"
+    dynamodb_table = "terraform-session-sep-state-lock"  # Lock Table
   }
 }
+
+
+# 1. You cannot use any variables in key

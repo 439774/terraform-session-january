3b3b43cb480e064d067fe6efd8aa0871d5b4d9f6
439774 <therealkimbladi@gmail.com>
2023-02-11 11:09:29 -0600

    fixing backend

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mybuterraform-session-septemeber-backend-alishercket"
+    bucket = "terraform-session-septemeber-backend-alisher"
     key    = "session-5/terraform.tfstate"  # Where does terraform need to store your file. Path Or Prefix
     region = "us-east-1"
   }

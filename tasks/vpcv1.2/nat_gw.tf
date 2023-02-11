355e2ba96b22b2828573ce3ac3b1c81a1ca1e9d6
439774 <therealkimbladi@gmail.com>
2023-02-11 15:59:46 -0600

    check

--- tasks/vpcv1.2/nat_gw.tf
+++ tasks/vpcv1.2/nat_gw.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "task_nat_gw" {
   allocation_id = aws_eip.vpc_eip.allocation_id
-  subnet_id     = element(aws_subnet.pub_sub_a, count.index)
+  subnet_id     = aws_subnet.pub_sub_a[0].id
   tags = {
     Name = "terra_nat_gw"
   }

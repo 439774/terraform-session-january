3c6417ff28c62cbf5bafac57bebf02e64fcd3eea
439774 <therealkimbladi@gmail.com>
2023-02-11 16:03:55 -0600

    check

--- tasks/vpcv1.2/priv_route_table_assoc.tf
+++ tasks/vpcv1.2/priv_route_table_assoc.tf
@@ -1,5 +1,4 @@
 resource "aws_route_table_association" "priv_a" {
-  count = length(aws_subnet.pub_sub_a)
-  subnet_id      = element(aws_subnet.priv_sub_a, count.index)
+  subnet_id      = aws_subnet.priv_sub_a[2].id
   route_table_id = aws_route_table.priv_route_table_task.id
 }
--- tasks/vpcv1.2/public_route_table_assoc.tf
+++ tasks/vpcv1.2/public_route_table_assoc.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "pub_a" {
-  count = length(aws_subnet.pub_sub_a)
-  subnet_id      = element(aws_subnet.pub_sub_a, count.index)
+  count = length(aws_subnet.pub_sub_a.id)
+  subnet_id      = element(aws_subnet.pub_sub_a.id, count.index)
   route_table_id = aws_route_table.pub_route_table_task.id
 }

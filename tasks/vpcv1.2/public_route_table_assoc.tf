9a1509d607c4df1b02504ff4f076fb0c3a84156a
439774 <therealkimbladi@gmail.com>
2023-02-11 16:11:09 -0600

    trying to fix

--- tasks/vpcv1.2/public_route_table_assoc.tf
+++ tasks/vpcv1.2/public_route_table_assoc.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "pub_a" {
-  subnet_id      = aws_subnet.pub_sub_a[0].id
+  subnet_id      = aws_subnet.pub_sub_a[2].id
   route_table_id = aws_route_table.pub_route_table_task.id
 }

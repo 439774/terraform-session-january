e6ec01b8817faf984b70226f1bd08fe62f3b30ce
439774 <therealkimbladi@gmail.com>
2023-02-11 11:14:18 -0600

    fixing variables

--- tasks/security-groupv1.1/security.tf
+++ tasks/security-groupv1.1/security.tf
@@ -6,35 +6,35 @@
         from_port        = var.port1
         to_port          = var.port1 # number
         protocol         = "${var.protocol}" # string
-        cidr_blocks      = [format("", var.cidr_blocks)] # list of strings
+        cidr_blocks      = [format("%s", var.cidr_blocks)] # list of strings
     }
 
     ingress {
         from_port        = var.port2
         to_port          = var.port2
         protocol         = "${var.protocol}" # string
-        cidr_blocks      = [format("", var.cidr_blocks)] # list of strings
+        cidr_blocks      = [format("%s", var.cidr_blocks)] # list of strings
     }
 
     ingress {
         from_port        = var.port3
         to_port          = var.port3
         protocol         = "${var.protocol}" # string
-        cidr_blocks      = [format("", var.cidr_blocks)] # list of strings
+        cidr_blocks      = [format("%s", var.cidr_blocks)] # list of strings
     }
 
     ingress {
         from_port        = var.port5
         to_port          = var.port5
         protocol         = "${var.protocol}" # string
-        cidr_blocks      = [format("", var.cidr_blocks)] # list of strings
+        cidr_blocks      = [format("%s", var.cidr_blocks)] # list of strings
     }
 
     egress {
         from_port        = 0
         to_port          = 0
         protocol         = "-1" # string
-        cidr_blocks      = [format("", var.cidr_blocks)] # list of strings
+        cidr_blocks      = [format("%s", var.cidr_blocks)] # list of strings
     }
     
     }

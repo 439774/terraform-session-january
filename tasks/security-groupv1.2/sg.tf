fa5a1dd6906781ca8324b3f7b258025e31fa1e78
439774 <therealkimbladi@gmail.com>
2023-02-11 13:53:07 -0600

    testing

--- tasks/security-groupv1.2/sg.tf
+++ tasks/security-groupv1.2/sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group_rule" "ingress" {
   count = length([ "${var.ports}", "${var.cidrs}" ])
   type              = "ingress"
   to_port           = element(var.ports, count.index) #22
-  protocol          = "tcp"
+  protocol          = "${var.protocol}"
   from_port         = element(var.ports, count.index) #22
   security_group_id = aws_security_group.main_sg.id
   cidr_blocks = [ format("%s", element(var.cidrs, count.index)) ]
@@ -16,7 +16,7 @@ resource "aws_security_group_rule" "ingress" {
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
   to_port           = 0
-  protocol          = "tcp"
+  protocol          = "${var.protocol}"
   from_port         = 0
   security_group_id = aws_security_group.main_sg.id
   cidr_blocks = [ format("%s", var.cidrs) ]

86b2f4e90267898463ba1f8a94fe8ea90ed2b9b5
439774 <therealkimbladi@gmail.com>
2023-02-11 16:22:25 -0600

    trying to fix

--- tasks/vpcv1.2/pub_sub.tf
+++ tasks/vpcv1.2/pub_sub.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "pub_sub_a" {
-  count = length(["${var.public_cidrs}", "${var.global_az}"])
+  count = length(var.public_cidrs)
   vpc_id     = aws_vpc.task_vpc.id
   cidr_block = format("%s", element(var.public_cidrs, count.index))
   availability_zone = format("%s", element(var.global_az, count.index))

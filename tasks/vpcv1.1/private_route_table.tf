e87f580065b46331a1610bcce5e5cf2725039193
439774 <therealkimbladi@gmail.com>
2023-02-10 23:48:37 -0600

    finished

--- tasks/vpcv1.1/private_route_table.tf
+++ tasks/vpcv1.1/private_route_table.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "priv_route_table_task" {
   vpc_id = aws_vpc.task_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "${var.open_cidr}"
     nat_gateway_id = aws_nat_gateway.task_nat_gw.id
   }
   tags = {

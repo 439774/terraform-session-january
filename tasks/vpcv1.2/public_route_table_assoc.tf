50be7fdb758dda6596f5a68612a2fca9273826fa
439774 <therealkimbladi@gmail.com>
2023-02-11 15:27:34 -0600

    checking

--- tasks/vpcv1.2/public_route_table_assoc.tf
+++ tasks/vpcv1.2/public_route_table_assoc.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "pub_a" {
-  count = aws_subnet.pub_sub_a
+  count = length(aws_subnet.pub_sub_a)
   subnet_id      = element(aws_subnet.pub_sub_a, count.index)
   route_table_id = aws_route_table.pub_route_table_task.id
 }

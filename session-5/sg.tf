fe7b91a505b38f8c199e425d46965f32ac2e2c56
439774 <therealkimbladi@gmail.com>
2023-02-11 13:07:55 -0600

    applying count index

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -6,9 +6,9 @@ resource "aws_security_group" "main_sg" {
 resource "aws_security_group_rule" "ingress" {
   count = 2
   type              = "ingress"
-  to_port           = element(var.ports, 0) #22
+  to_port           = element(var.ports, count.index) #22
   protocol          = "tcp"
-  from_port         = element(var.ports, 0) #22
+  from_port         = element(var.ports, count.index) #22
   security_group_id = aws_security_group.main_sg.id
   cidr_blocks = [ "0.0.0.0/0" ]
 }
@@ -21,6 +21,8 @@ resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.main_sg.id
   cidr_blocks = [ "0.0.0.0/0" ]
 }
+
+# count.index = count the indexes
 
 # Corrections:
 # 1. Lower case

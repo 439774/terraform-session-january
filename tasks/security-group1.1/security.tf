33691bada0f91f53f36d2a80e6dd786f3907d538
439774 <therealkimbladi@gmail.com>
2023-02-10 23:26:07 -0600

    set up

---
+++ tasks/security-group1.1/security.tf
@@ -0,0 +1,46 @@
+    resource "aws_security_group" "CreateSG" {   
+    name        = "main"
+    description = "Allow SSH inbound traffic"
+
+    ingress {
+        from_port        = 22
+        to_port          = 22 # number
+        protocol         = "tcp" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
+
+    ingress {
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
+
+    ingress {
+        from_port        = 443
+        to_port          = 443
+        protocol         = "tcp" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
+
+    ingress {
+        from_port        = 3306
+        to_port          = 3306
+        protocol         = "tcp" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
+    
+    }
+
+
+# Corrections:
+# 1. Lower case
+# 2. _ instead of -
+# 3. Proper name for the second_label

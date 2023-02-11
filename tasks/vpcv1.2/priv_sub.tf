50b6d74fb5884712a35f4778d4e1310d5c90a1c9
439774 <therealkimbladi@gmail.com>
2023-02-11 16:27:38 -0600

    finished

--- tasks/vpcv1.2/priv_sub.tf
+++ tasks/vpcv1.2/priv_sub.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "priv_sub_a" {
-  count = length(["${var.private_cidrs}", "${var.global_az}"])
+  count = length("${var.private_cidrs}")
   vpc_id     = aws_vpc.task_vpc.id
   cidr_block = format("%s", element(var.private_cidrs, count.index))
   availability_zone = format("%s", element(var.global_az, count.index))

e614065bf227df2d29847be29ef85ffb660d5a78
439774 <therealkimbladi@gmail.com>
2023-02-13 10:42:30 -0600

    changes

--- tasks/vpcv1.2/priv_route_table_assoc.tf
+++ tasks/vpcv1.2/priv_route_table_assoc.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "priv_a" {
-  subnet_id      = aws_subnet.priv_sub_a[2].id
+  subnet_id      = aws_subnet.priv_sub_a[*].id
   route_table_id = aws_route_table.priv_route_table_task.id
 }

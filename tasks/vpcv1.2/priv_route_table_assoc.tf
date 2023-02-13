ae6a855d86ff55288232aa9a1fac3bd4b81f7972
439774 <therealkimbladi@gmail.com>
2023-02-13 10:47:04 -0600

    finished

--- tasks/vpcv1.2/priv_route_table_assoc.tf
+++ tasks/vpcv1.2/priv_route_table_assoc.tf
@@ -1,4 +1,5 @@
 resource "aws_route_table_association" "priv_a" {
-  subnet_id      = aws_subnet.priv_sub_a[*].id
+  count = length(aws_subnet.priv_sub_a[*])
+  subnet_id      = aws_subnet.priv_sub_a[count.index].id
   route_table_id = aws_route_table.priv_route_table_task.id
 }

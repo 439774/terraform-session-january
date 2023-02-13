13ef4d3b51a0dae23ea5d925934300361adfccc5
439774 <therealkimbladi@gmail.com>
2023-02-13 17:55:44 -0600

    finished

--- personal_tasks/auto_scaling_group/variables.tf
+++ personal_tasks/auto_scaling_group/variables.tf
@@ -1,26 +1,3 @@
 variable "aws_region" {    
     default = "us-east-1"
 }
-
-variable "open_cidr" {
-  type = list(string)
-  default = ["0.0.0.0/0", "10.0.0.0/16"]
-}
-
-variable "public_cidrs" {
-    type = list(string)
-    description = "List of cidrs"
-    default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ] # index numbers 0 1 ...
-}
-
-variable "private_cidrs" {
-    type = list(string)
-    description = "List of cidrs"
-    default = [ "10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24" ] # index numbers 0 1 ...
-}
-
-variable "global_az" {
-    type = list(string)
-    description = "List of cidrs"
-    default = [ "us-east-1a", "us-east-1b", "us-east-1c" ] # index numbers 0 1 ...
-}

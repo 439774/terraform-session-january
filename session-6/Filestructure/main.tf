4c1cb16ab54719eca2a66e8398055bb8f49bf45e
439774 <therealkimbladi@gmail.com>
2023-02-13 19:00:44 -0600

    testing

--- session-6/Filestructure/main.tf
+++ session-6/Filestructure/main.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "main" {
     count = length(var.env[*])
     name = format("%s-sqs", var.env[count.index])
     tags = {
-        enviorment = var.env[0]
+        enviorment = format("%s", var.env[count.index])
         Name = format("%s-sqs", var.env[count.index])  # dev-sqs, qa-sqs, stage-sqs, prod-sqs
     }     
 }

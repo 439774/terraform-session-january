745836ee91f6b91205896562f4fcb79fb49b1574
439774 <therealkimbladi@gmail.com>
2023-02-13 20:12:40 -0600

    checking

--- session-6/Filestructure/terraform.sh
+++ session-6/Filestructure/terraform.sh
@@ -11,8 +11,7 @@ terraform init
 terraform plan -var-file=$ENV.tfvars
 
 read -p "You sure you wanna apply it?" apply 
-if [yes];
-then terraform apply -var-file=$ENV.tfvars -auto-approve
+if [yes]; then terraform apply -var-file=$ENV.tfvars -auto-approve
 else echo "Terraform is not gonna apply"
 fi
 

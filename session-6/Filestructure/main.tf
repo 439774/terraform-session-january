a1541d4a0c4b8e36dc227e295d29e2f65b0d4ea9
439774 <therealkimbladi@gmail.com>
2023-02-13 18:43:57 -0600

    fixed syntax error

--- session-6/Filestructure/main.tf
+++ session-6/Filestructure/main.tf
@@ -2,6 +2,6 @@ resource "aws_sqs_queue" "main" {
     name = "sqs"
     tags = {
         enviorment = var.env
-        Name = format("%s-sqs". var.env)  # dev-sqs, qa-sqs, stage-sqs, prod-sqs
+        Name = format("%s-sqs", var.env)  # dev-sqs, qa-sqs, stage-sqs, prod-sqs
     }     
 }

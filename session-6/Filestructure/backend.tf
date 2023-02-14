3627c2880521c049ddc709e690f24323e512bb3d
439774 <therealkimbladi@gmail.com>
2023-02-13 20:01:10 -0600

    fixing names

--- session-6/Filestructure/backend.tf
+++ session-6/Filestructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-session-septemeber-backend-alisher"
-    key    = "session-6/filestructure/dev/terraform.tfstate"  # Where does terraform need to store your file. Path Or Prefix
+    key    = "session-6/filestructure/_env_/terraform.tfstate"  # Where does terraform need to store your file. Path Or Prefix
     region = "us-east-1"
     dynamodb_table = "terraform-session-sep-state-lock"  # Lock Table
   }

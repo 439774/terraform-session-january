ad32ed36074b849ed63de5f6ab83d4d202a59389
439774 <therealkimbladi@gmail.com>
2023-02-15 21:15:47 -0600

    notes

--- session-7/backend/main.tf
+++ session-7/backend/main.tf
@@ -1,20 +1,50 @@
 resource "aws_db_instance" "main" {
   allocated_storage    = 10
-  identifier           = replace(local.name, "resource", "rds") # name for RDS instnace
-  db_name              = "mydb" # database name inside the RDS instance
+  identifier           = replace(local.name, "resource", "rds")  # name for RDS instance
+  db_name              = "mydb"                 # database name inside the RDS instance
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  username             = "admin"
+  username             = "admin" # admin user
   password             = random_password.db_password.result
-  skip_final_snapshot  = var.env != "prod" ? false : true  # it will create a snapshot
-  final_snapshot_identifier = var.env != "prod" ? null : "${var.env}-db-final-snapshot"
+  skip_final_snapshot  = var.env != "prod" ? true : false      # if env = dev, true
+  final_snapshot_identifier = var.env != "prod" ?  null : "${var.env}-db-final-snapshot"  #if env = dev, null
 }
+
+# if environment is prod, set to false
+# but for the other environments, set to true 
+
+# condtion ? true_value : false_value
 
 resource "random_password" "db_password" {
-  length = 20
-  special = true
-  override_special = "%@"
+    length = 20
+    special = true
+    override_special = "%@"
 }
 
-# var.env == prod ? false : true
+# Equality Operators:
+# a == a 
+# > true
+
+# a != b 
+# > true
+
+# a != a
+# > false 
+
+# Conditional Expression:
+
+# Syntax: 
+# condition ? first_value : second_value
+
+# Example:
+
+# car == car ? true_honda : false_toyota
+# > 
+
+# car == var ? honda : toyota
+# > 
+
+# true == false ? false : true
+# > true
+

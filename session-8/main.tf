80baa5edd1bb59a85d9a4429fbcdda0c4e89488c
439774 <therealkimbladi@gmail.com>
2023-02-20 19:51:20 -0600

    fixied

--- session-8/main.tf
+++ session-8/main.tf
@@ -15,14 +15,14 @@ resource "aws_instance" "first_ec2" {
       private_key = file("~/.ssh/id_rsa")
     }
   }
-  provisioner "remote_exec" {
+  provisioner "remote-exec" {
     inline = [
         "sudo yum install httpd -y",
         "sudo systemctl enable httpd",
         "sudo systemctl start httpd",
         "sudo cp /tmp/index.html /var/www/html/index.html"
     ]
-        connection {
+  connection {
       type = "ssh"
       user = "ec2-user"
       host = self.public_ip

505e7b62bcf198633445906a98431cdeeb8af2d1
439774 <therealkimbladi@gmail.com>
2023-02-20 18:43:21 -0600

    fixing

--- session-8/sg.tf
+++ session-8/sg.tf
@@ -1,27 +1,30 @@
-resource "aws_security_group" "main_sg" {   
+    resource "aws_security_group" "main_sg" {   
     name        = "main"
     description = "Allow SSH inbound traffic"
-}
 
-resource "aws_security_group_rule" "ingress" {
-  type              = "ingress"
-  to_port           = 22
-  protocol          = "tcp"
-  from_port         = 22
-  security_group_id = aws_security_group.main_sg.id
-  cidr_blocks = [ "0.0.0.0/0" ]
-}
+    ingress {
+        from_port        = 22
+        to_port          = 22 # number
+        protocol         = "tcp" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
 
-resource "aws_security_group_rule" "ingress" {
-  type              = "egress"
-  to_port           = 80
-  protocol          = "tcp"
-  from_port         = 80
-  security_group_id = aws_security_group.main_sg.id
-  cidr_blocks = [ "0.0.0.0/0" ]
-}
+    ingress {
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
 
-# count.index = count the indexes
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1" # string
+        cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    }
+    
+    }
+
 
 # Corrections:
 # 1. Lower case

6f8c90daf8d1299a78b7a967f8ee0f0b2eb61e90
439774 <therealkimbladi@gmail.com>
2023-02-27 19:22:27 -0600

    changes

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,6 +1,21 @@
+# Calling a child module remotely 
+# Github
 module "ec2" {
-    source ="github.com/AlisherMuhtarov/terraform-session-january/modules/ec2"
+    source = "github.com/AlisherMuhtarov/terraform-session-january/modules/ec2"
     env = "dev"
-    ami = "ami-0aa7d40eeae50c9a9"
+    ami = "ami-0aa7d40eeae50c9a9" 
     instance_type = "t2.micro"
 }
+
+# Terraform Registry 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.0"
+}
+# github.com = Domain Name (SCM)
+# /aKumoSolutions = Organization
+# /terraform-session-january = Repository
+# /tree = 
+# /main = Branch (default)
+# /modules = Subdirectory
+# /ec2 = Subdirectory
